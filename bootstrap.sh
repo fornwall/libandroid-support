c580b518f5803ddf468e78e0c6c25d01be9f8ed1
Fredrik Fornwall <fredrik@fornwall.net>
2014-01-21 20:16:50 +0100

    Add distcheck target to bootstrap.sh.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,13 @@
+#!/bin/sh
+
+set -u -e
+
 autoreconf --force --install
+
+if test "$1" = "distcheck"; then
+	echo "Making a distcheck in ./build/ ..."
+	mkdir build
+	cd build
+	../configure --host=arm-linux-androideabi
+	make distcheck DISTCHECK_CONFIGURE_FLAGS="--host=arm-linux-androideabi"
+fi
