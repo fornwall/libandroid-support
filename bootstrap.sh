15bcec996f447cd6e914b406b45aa010a21f2cb5
Fredrik Fornwall <fredrik@fornwall.net>
2014-01-29 22:08:59 +0100

    Fix the bootstrap.sh script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-set -u -e
+set -e
 
 autoreconf --force --install
 
 if test "$1" = "distcheck"; then
 	echo "Making a distcheck in ./build/ ..."
+	rm -Rf build
 	mkdir build
 	cd build
 	../configure --host=arm-linux-androideabi
